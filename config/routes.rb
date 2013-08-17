95e3e0bff9821bece5e47954bed20cf72bdcf6fd
Masaaki Hatano <masaonly1@gmail.com>
2013-08-18 00:49:21 +0900

    August 17th

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Omrails::Application.routes.draw do
+  devise_for :users
   get 'about' => 'pages#about'
-
+  
   root :to => 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
